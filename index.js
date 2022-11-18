91dc3776cf320d19a8bd6c372e294acb9a0cd889
MahekUnnisa <mahekunnisa786@gmail.com>
2022-11-18 23:44:54 +0530

    Homepage-view done

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'routes/home.html'));
 });
 
+app.get('/anima', (req, res)=>{
+    res.status(200);
+    res.sendFile(path.join(__dirname, 'routes/anima.html'));
+});
+
 //the server will listen on port 3000
 //it will give a message in the console accordingly
 app.listen(PORT, (error) => {
